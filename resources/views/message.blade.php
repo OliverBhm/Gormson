e50afce83d9d0a0816076ce0188db01627d299fa
oliver.boehm@plentymarkets.de <>
2020-09-25 15:11:24 +0200

    fixed formatting

--- resources/views/message.blade.php
+++ resources/views/message.blade.php
@@ -2,7 +2,7 @@
 @foreach($dates as $date)
 {{$date['employee']['first_name']}} {{$date['employee']['last_name']}}@if($date['absence_type'] == 'Half a day') for {{$date['absence_type']}}@endif from: *{{$date['absence_begin']}}* until: *{{$date['absence_end']}}*
 @if(count($date['employee']['substitutes']) >= 1)
-{{'Please refer to: '}}@foreach($date['employee']['substitutes'] as $substitute) {{$substitute['first_name']}} {{$substitute['last_name']}}@if(isset(next($date['employee']['substitutes'])['first_name'])),@endif
+{{'Please refer to: '}}@foreach($date['employee']['substitutes'] as $substitute){{$substitute['first_name']}} {{$substitute['last_name']}}@if(isset(next($date['employee']['substitutes'])['first_name'])), @endif
 @endforeach
 
 @endif
