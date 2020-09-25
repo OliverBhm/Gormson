6ad92044184f49e24cbb3d84f2fc36bd85bfadfd
oliver.boehm@plentymarkets.de <>
2020-09-25 14:10:20 +0200

    separate substitutes by comma if more then one is present

--- resources/views/dates.blade.php
+++ resources/views/dates.blade.php
@@ -1,8 +1,9 @@
 *{{$header}}*
 @foreach($dates as $date)
 {{$date['employee']['first_name']}} {{$date['employee']['last_name']}}@if($date['absence_type'] == 'Half a day') for {{$date['absence_type']}}@endif until: *{{$date['absence_end']}}*
-@if(count($date['employee']['substitutes']) == 1)
-{{'Please refer to: '}}@foreach($date['employee']['substitutes'] as $substitute){{$substitute['first_name']}} {{$substitute['last_name']}}
+@if(count($date['employee']['substitutes']) >= 1)
+{{'Please refer to: '}}@foreach($date['employee']['substitutes'] as $substitute){{$substitute['first_name']}} {{$substitute['last_name']}}@if(isset(next($date['employee']['substitutes'])['first_name'])),@endif
+
 @endforeach
 @endif
 @endforeach
